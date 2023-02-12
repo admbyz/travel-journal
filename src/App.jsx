79d53abcc6e390580ebfee44319f6c41b5e6efa7
admbyz <admbeyaz@gmail.com>
2023-02-12 23:18:35 +0300

    add styling

--- src/App.jsx
+++ src/App.jsx
@@ -3,11 +3,17 @@ import Content from "./components/Content";
 import data from "./data";
 
 function App() {
-  const contentData = data.map((item) => <Content key={item.id} {...item} />);
+  const contentData = data.map((item, i) => (
+    <div key={item.id}>
+      <Content {...item} />
+      {i === data.length - 1 ? null : <hr />}
+    </div>
+  ));
+
   return (
     <div className="App">
       <Header />
-      {contentData}
+      <main className="location">{contentData}</main>
     </div>
   );
 }
